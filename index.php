ed2c481388ed1b2e2bb6364933802955316220e5
IMtzAgr2022 <106554635+IMtzAgr2022@users.noreply.github.com>
2023-09-24 22:10:31 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control de Inventarios</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Control de Inventarios</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="productos.php">Productos</a></li>
+                <li><a href="#">Compras</a></li>
+                <li><a href="#">Ventas</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <!-- Contenido principal de la página -->
+        <section>
+            <h2>Últimas Compras</h2>
+            <!-- Coloca aquí la lista de las últimas compras -->
+        </section>
+        
+        <section>
+            <h2>Productos Destacados</h2>
+            <!-- Coloca aquí la lista de productos destacados -->
+        </section>
+    </main>
+    
+    <footer>
+        <div class="contact-info">
+            <h3>Contacto</h3>
+            <p>Teléfono: (123) 456-7890</p>
+            <p>Email: info@tuempresa.com</p>
+        </div>
+    </footer>
+</body>
+</html>
